e79a01fed419549a814edd79b242612b300ecc37
Mohammed Mubarak <modymu9@gmail.com>
2023-11-28 16:07:59 +0200

    Install Nginx web server (w/ Puppet)

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,62 @@
+exec { 'update packages':
+  command => 'apt-get update',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
+
+package { 'nginx':
+  ensure     => 'installed',
+}
+
+exec { 'chown html folder':
+  command => 'sudo chown -R "\$USER":"\$USER" /var/www/html;',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
+
+exec { 'chmod www folder':
+  command => 'chmod -R 755 /var/www',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
+
+file { '/var/www/html/index.html':
+  content => "Hello World!\n",
+}
+
+file { '/var/www/html/error-404.html':
+  content => "Ceci n'est pas une page\n",
+}
+
+file { 'Nginx default config file':
+  ensure  => file,
+  path    => '/etc/nginx/sites-enabled/default',
+  content => 'server {
+        listen 80 default_server;
+        listen [::]:80 default_server;
+        root /var/www/html;
+
+        index index.html index.htm index.nginx-debian.html;
+
+        server_name _;
+
+        location / {
+                # First attempt to serve request as file, then
+                # as directory, then fall back to displaying a 404.
+                try_files $uri $uri/ =404;
+        }
+
+        location /redirect_me {
+                return 301 $scheme://www.youtube.com/watch?v=QH2-TGUlwu4;
+        }
+        error_page 404 /error-404.html;
+}
+',
+}
+
+exec { 'restart service':
+  command => 'service nginx restart',
+  path    => '/usr/bin:/usr/sbin:/bin',
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
