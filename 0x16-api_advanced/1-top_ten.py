f2805ec0258d9e7465641725343fc4590a57617b
Mohammed Mubarak <modymu9@gmail.com>
2024-01-17 06:08:13 +0200

    Top Ten

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""List to 10 hot articles."""
+import requests
+
+
+def top_ten(subreddit):
+    """Hot subreddits."""
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+        "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 "
+        "Safari/537.36 Edg/120.0.0.0"
+    }
+    with requests.Session() as s:
+        url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+        try:
+            sub = s.get(url, headers=headers).json()
+            hotest = sub["data"]["children"]
+        except Exception:
+            print(None)
+            return
+        for i in range(10):
+            print(hotest[i]["data"]["title"])
